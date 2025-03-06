73223201f722a478de28da78d781c684c972cadc
Sherdrom <sherd_c@outlook.com>
2025-03-06 21:52:06 +0800

    修复霰弹枪上弹的一个bug

--- Lua/Autorun/ReloadShotGun/M590.lua
+++ Lua/Autorun/ReloadShotGun/M590.lua
@@ -187,6 +187,7 @@ Hook.Add("M590Reload", "PrecisionReloadHandler", function(effect, deltaTime, ite
             item.IsShootable = true
         end, 100)
         resetAnimation(item)
+        cancelReload(item.ID)
     end, disableShootTime * 1000)
     
     -- 完成时清理
--- Lua/Autorun/ReloadShotGun/supernova.lua
+++ Lua/Autorun/ReloadShotGun/supernova.lua
@@ -187,6 +187,7 @@ Hook.Add("supernovaReload", "PrecisionReloadHandler", function(effect, deltaTime
             item.IsShootable = true
         end, 100)
         resetAnimation(item)
+        cancelReload(item.ID)
     end, disableShootTime * 1000)
     
     -- 完成时清理
