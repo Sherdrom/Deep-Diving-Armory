6394bfd18712a6fd7cf3ef6087a267edd4ec8eb8
peach <1902445394@qq.com>
2025-03-22 22:44:22 +0800

    38大盖完成

---
+++ Lua/Autorun/ReloadSniperRifle/TYPE_38.lua
@@ -0,0 +1,9 @@
+if SERVER then return end
+
+Hook.Add("TYPE_38BoltOutSprite", "TYPE_38BoltOut", function(_, _, item)
+    item.Sprite.SourceRect=Rectangle(15,772,649,118)
+end)
+
+Hook.Add("TYPE_38BoltInSprite", "TYPE_38BoltIn", function(_, _, item)
+    item.Sprite.SourceRect=Rectangle(15,664,649,92)
+end)
