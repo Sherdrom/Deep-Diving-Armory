99c53fd44eddc54bbf2f4831aaaf541c2f0f8973
peach <1902445394@qq.com>
2025-03-22 23:57:05 +0800

    38,98k,中正步枪完成

--- Lua/Autorun/ReloadSniperRifle/TYPE_38.lua
+++ Lua/Autorun/ReloadSniperRifle/TYPE_38.lua
@@ -7,3 +7,8 @@ end)
 Hook.Add("TYPE_38BoltInSprite", "TYPE_38BoltIn", function(_, _, item)
     item.Sprite.SourceRect=Rectangle(15,664,649,92)
 end)
+
+Hook.Add("TYPE_38BrokenIcon", "TYPE_38BrokenIcon", function(_, _, item)
+    item.Prefab.InventoryIcon.SourceRect = Rectangle(561,160,19,68)
+    item.Prefab.InventoryIcon.Depth = 0.65
+end)
