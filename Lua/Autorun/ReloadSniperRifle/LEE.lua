b6c7becb97cfd185ffabf8a194d4f9cddcc0133f
Sherdrom <525826386@qq.com>
2025-03-23 19:56:25 +0800

    删掉多余的Hook

--- Lua/Autorun/ReloadSniperRifle/LEE.lua
+++ Lua/Autorun/ReloadSniperRifle/LEE.lua
@@ -7,8 +7,3 @@ end)
 Hook.Add("LEEBoltInSprite", "LEEBoltIn", function(_, _, item)
     item.Sprite.SourceRect=Rectangle(9,429,576,108)
 end)
-
-Hook.Add("LEEBrokenIcon", "LEEBrokenIcon", function(_, _, item)
-    item.Prefab.InventoryIcon.SourceRect = Rectangle(558,76,25,70)
-    item.Prefab.InventoryIcon.Depth = 0.65
-end)
