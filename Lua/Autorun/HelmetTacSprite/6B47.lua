cce97f23dbaa61ec53e2022457402f807fedda9a
peach <1902445394@qq.com>
2025-03-30 19:27:08 +0800

    头盔彻底修复

--- Lua/Autorun/HelmetTacSprite/6B47.lua
+++ Lua/Autorun/HelmetTacSprite/6B47.lua
@@ -24,6 +24,7 @@ local isExecuted={}
 
 Hook.Add("SixBFortySeven_Origin", function(_, _, item)
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(9,9,98,93)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.55,0.62)
     -- 移除战术设备时应当清空所有的执行状态
@@ -35,6 +36,7 @@ end)
 Hook.Add("SixBFortySeven_Thermal_On", function(_, _, item)
     if isExecuted.SixBFortySeven_Thermal_On then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(14,114,141,93)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.42,0.58)
     isExecuted.SixBFortySeven_Thermal_On = true
@@ -44,6 +46,7 @@ end)
 Hook.Add("SixBFortySeven_Thermal_Off", function(_, _, item)
     if isExecuted.SixBFortySeven_Thermal_Off then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(178,7,130,121)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.45,0.7)
     isExecuted.SixBFortySeven_Thermal_Off = true
@@ -53,6 +56,7 @@ end)
 Hook.Add("SixBFortySeven_Night_On", function(_, _, item)
     if isExecuted.SixBFortySeven_Night_On then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     SoundPlayer.PlaySound(WEAR_CONFIG.Sound.nightSound.turnOn, item.WorldPosition, WEAR_CONFIG.Sound.nightSound.gain, WEAR_CONFIG.Sound.nightSound.range, WEAR_CONFIG.Sound.nightSound.frequencyMultiplier)
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(12,219,142,93)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.42,0.62)
@@ -63,6 +67,7 @@ end)
 Hook.Add("SixBFortySeven_Night_Off", function(_, _, item)
     if isExecuted.SixBFortySeven_Night_Off then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(334,16,153,113)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.38,0.7)
     isExecuted.SixBFortySeven_Night_Off = true
@@ -72,6 +77,7 @@ end)
 Hook.Add("SixBFortySeven_Health_On", function(_, _, item)
     if isExecuted.SixBFortySeven_Health_On then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(9,324,124,94)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.45,0.62)
     isExecuted.SixBFortySeven_Health_On = true
@@ -81,6 +87,7 @@ end)
 Hook.Add("SixBFortySeven_Health_Off", function(_, _, item)
     if isExecuted.SixBFortySeven_Health_Off then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(177,143,124,112)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.45,0.7)
     isExecuted.SixBFortySeven_Health_Off = true
--- Lua/Autorun/HelmetTacSprite/GHOST.lua
+++ Lua/Autorun/HelmetTacSprite/GHOST.lua
@@ -24,6 +24,7 @@ local isExecuted={}
 
 Hook.Add("GHOST_Origin", function(_, _, item)
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(5,4,107,98)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.55,0.67)
     -- 移除战术设备时应当清空所有的执行状态
@@ -35,6 +36,7 @@ end)
 Hook.Add("GHOST_Thermal_On", function(_, _, item)
     if isExecuted.GHOST_Thermal_On then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(4,112,143,98)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.42,0.63)
     isExecuted.GHOST_Thermal_On = true
@@ -44,6 +46,7 @@ end)
 Hook.Add("GHOST_Thermal_Off", function(_, _, item)
     if isExecuted.GHOST_Thermal_Off then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(180,8,136,125)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.45,0.7)
     isExecuted.GHOST_Thermal_Off = true
@@ -53,6 +56,7 @@ end)
 Hook.Add("GHOST_Night_On", function(_, _, item)
     if isExecuted.GHOST_Night_On then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     SoundPlayer.PlaySound(WEAR_CONFIG.Sound.nightSound.turnOn, item.WorldPosition, WEAR_CONFIG.Sound.nightSound.gain, WEAR_CONFIG.Sound.nightSound.range, WEAR_CONFIG.Sound.nightSound.frequencyMultiplier)
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(6,229,140,99)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.42,0.66)
@@ -63,6 +67,7 @@ end)
 Hook.Add("GHOST_Night_Off", function(_, _, item)
     if isExecuted.GHOST_Night_Off then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(334,18,161,115)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.38,0.7)
     isExecuted.GHOST_Night_Off = true
@@ -72,6 +77,7 @@ end)
 Hook.Add("GHOST_Health_On", function(_, _, item)
     if isExecuted.GHOST_Health_On then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(12,343,127,98)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.5,0.65)
     isExecuted.GHOST_Health_On = true
@@ -81,6 +87,7 @@ end)
 Hook.Add("GHOST_Health_Off", function(_, _, item)
     if isExecuted.GHOST_Health_Off then return end
     local itemComponent = item.GetComponentString("Wearable")
+    if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(179,154,135,111)
     itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.45,0.7)
     isExecuted.GHOST_Health_Off = true
