3f4b6cb8bb122991a93244ced65023530ac524dd
Sherdrom <525826386@qq.com>
2025-05-08 23:04:58 +0800

    重新命名Altyn lua并删除了一些不必要的using

--- .AssemblyCSharpSource/DeepVisionPatch/SharedProject/SharedSource/Plugin.cs
+++ .AssemblyCSharpSource/DeepVisionPatch/SharedProject/SharedSource/Plugin.cs
@@ -5,7 +5,6 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using HarmonyLib;
 using Barotrauma;
-using Microsoft.Xna.Framework;
 
 [assembly: IgnoresAccessChecksTo("Barotrauma")]
 [assembly: IgnoresAccessChecksTo("DedicatedServer")]
