49659988e50ff112892c26b5bc31d2f42ff77239
NotAlwaysTrue <2136846186@qq.com>
2025-07-01 22:19:37 +0800

    修正了SR-25 M993穿甲小于预期 暂时移除了导弹系统和XM-25有关代码

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -5,9 +5,9 @@ if SERVER or not Game.IsMultiplayer then
 
     dofile(Deep_Lua.Path .. "/Lua/Scripts/armorconfigs.lua")--Armor configs
     dofile(Deep_Lua.Path .. "/Lua/Scripts/armorMain.lua")--Armor Main
-    dofile(Deep_Lua.Path .. "/Lua/Scripts/missileconfigs.lua")--Missile configs, will load before missileframework
-    dofile(Deep_Lua.Path .. "/Lua/Scripts/missileframework.lua")--Missile Main, will be override if Framework is already exist
+    --dofile(Deep_Lua.Path .. "/Lua/Scripts/missileconfigs.lua")--Missile configs, will load before missileframework
+    --dofile(Deep_Lua.Path .. "/Lua/Scripts/missileframework.lua")--Missile Main, will be override if Framework is already exist
 
 end
 
-dofile(Deep_Lua.Path .. "/Lua/Scripts/AirBurst.lua")--Airburst stuff, work both side
+--dofile(Deep_Lua.Path .. "/Lua/Scripts/AirBurst.lua")--Airburst stuff, work both side
