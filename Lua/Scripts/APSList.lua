7c7e2ac79d788f1790e59aadd3c74516ac1ba6b2
NotAlwaysTrue <2136846186@qq.com>
2025-07-25 05:03:39 +0800

    添加APS待机提示音 添加APS激活提示音

--- Lua/Scripts/APSList.lua
+++ Lua/Scripts/APSList.lua
@@ -7,6 +7,9 @@ Deep_Lua.APS = {
         action = function(activeapsdata)
             if activeapsdata.triggered == nil then activeapsdata.triggered = false end
             Game.Explode(activeapsdata.apstarget.WorldPosition, 50, 30, 50, 50, 50, 0, 0)
+            activeapsdata.apsitem.GetComponentString("LightComponent").pulseAmount = 1.0
+            activeapsdata.apsitem.GetComponentString("LightComponent").pulseFrequency = 2.5
+            activeapsdata.apsitem.GetComponentString("LightComponent").Rotation = 1
             if Deep_Lua.HF.DoChance(activeapsdata.probability) then
                 Entity.Spawner.AddItemToRemoveQueue(activeapsdata.apstarget)
             end
@@ -14,6 +17,9 @@ Deep_Lua.APS = {
                 activeapsdata.triggered = true
                 Timer.Wait(function()
                     activeapsdata.apsitem.Condition = 0
+                    activeapsdata.apsitem.GetComponentString("LightComponent").pulseAmount = 0.0
+                    activeapsdata.apsitem.GetComponentString("LightComponent").pulseFrequency = 0
+                    activeapsdata.apsitem.GetComponentString("LightComponent").Rotation = 0
                 end,5000)
                 Timer.Wait(function()
                     activeapsdata.apsitem.Condition = 100
