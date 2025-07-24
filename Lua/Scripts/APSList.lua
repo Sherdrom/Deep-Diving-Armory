c29f706d815e048018bd04a6d79a150b3320918d
NotAlwaysTrue <2136846186@qq.com>
2025-07-24 14:55:38 +0800

    修改了APS(原型)运行代码 添加了APS(原型)贴图 APS现在将不会在物品栏内触发 APS将不会把物品栏内物品视为有效目标

--- Lua/Scripts/APSList.lua
+++ Lua/Scripts/APSList.lua
@@ -1,15 +1,22 @@
 Deep_Lua.APS = {
     defaultAPS = {
         minVelocity = 5,
-        maxVelocity = 40,
+        maxVelocity = 30,
         minsize = 0,
-        probability = 100,
+        probability = 0.95,
         action = function(item,target,prob)
             Game.Explode(target.WorldPosition,50,15,10,10,10,0,0)
             if Deep_Lua.HF.DoChance(prob) then
-                target.Condition = -100
+                Entity.Spawner.AddItemToRemoveQueue(target)
+                --target.Condition = -100
+                --target.body.LinearVelocity = target.body.LinearVelocity * 0.2
             end
             item.Condition = 0
+            Timer.Wait(
+                function()
+                    item.Condition = 100
+                end
+            ,8000)
         end
     }
 }
--- Lua/Scripts/deployables.lua
+++ Lua/Scripts/deployables.lua
@@ -25,7 +25,7 @@ Hook.Add("Deep_APS", "Deep_APS",
     function(effect, deltaTime, item, targets, worldPosition)
         local apsinfo = Deep_Lua.APS[item.Prefab.Identifier.Value] or Deep_Lua.APS.defaultAPS
         for target in targets do
-            if LuaUserData.IsTargetType(target, "Barotrauma.Item") and target.Condition > 0 then
+            if LuaUserData.IsTargetType(target, "Barotrauma.Item") and target.Condition > 0 and target.ParentInventory == nil then
                 if target.GetComponentString("Throwable") or target.GetComponentString("Projectile") then
                     if target.body.Height * target.body.Width >= apsinfo.minsize and (target.body.LinearVelocity.Length() >= apsinfo.minVelocity and target.body.LinearVelocity.Length() <= apsinfo.maxVelocity) then                    --Ah fuck those explosion based detection. We only care about speed and size
                         if Submarine.CheckVisibility(item.SimPosition,target.SimPosition,false,false,true,true,true) == nil then
