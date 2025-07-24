ce1ddbaf707d7cc47959011b727ca76f19d7accd
NotAlwaysTrue <2136846186@qq.com>
2025-07-24 21:17:48 +0800

    修改APS(原型)生效效果: 生效5秒 冷却15秒

--- Lua/Scripts/APSList.lua
+++ Lua/Scripts/APSList.lua
@@ -4,19 +4,22 @@ Deep_Lua.APS = {
         maxVelocity = 30,
         minsize = 0,
         probability = 0.95,
-        action = function(item,target,prob)
-            Game.Explode(target.WorldPosition,50,15,10,10,10,0,0)
-            if Deep_Lua.HF.DoChance(prob) then
-                Entity.Spawner.AddItemToRemoveQueue(target)
-                --target.Condition = -100
-                --target.body.LinearVelocity = target.body.LinearVelocity * 0.2
+        action = function(activeapsdata)
+            if activeapsdata.triggered == nil then activeapsdata.triggered = false end
+            Game.Explode(activeapsdata.apstarget.WorldPosition, 50, 30, 50, 50, 50, 0, 0)
+            if Deep_Lua.HF.DoChance(activeapsdata.probability) then
+                Entity.Spawner.AddItemToRemoveQueue(activeapsdata.apstarget)
             end
-            item.Condition = 0
-            Timer.Wait(
-                function()
-                    item.Condition = 100
-                end
-            ,8000)
+            if (not activeapsdata.triggered == true) then
+                activeapsdata.triggered = true
+                Timer.Wait(function()
+                    activeapsdata.apsitem.Condition = 0
+                end,5000)
+                Timer.Wait(function()
+                    activeapsdata.apsitem.Condition = 100
+                    activeapsdata.triggered = false
+                end,20000)
+            end
         end
     }
 }
