3367bafaf7fcfefd4bbde0c656ec5503cb8c5053
NotAlwaysTrue <2136846186@qq.com>
2025-07-25 09:01:52 +0800

    添加了服务器用多端同步代码

--- Lua/Scripts/APSList.lua
+++ Lua/Scripts/APSList.lua
@@ -7,8 +7,15 @@ Deep_Lua.APS = {
         action = function(activeapsdata)
             if activeapsdata.triggered == nil then activeapsdata.triggered = false end
             Game.Explode(activeapsdata.apstarget.WorldPosition, 50, 30, 50, 50, 50, 0, 0)
-            activeapsdata.apsitem.GetComponentString("LightComponent").pulseAmount = 1.0
-            activeapsdata.apsitem.GetComponentString("LightComponent").pulseFrequency = 2.5
+            local light = activeapsdata.apsitem.GetComponentString("LightComponent")
+            light.pulseAmount = 1.0
+            light.pulseFrequency = 2.5
+            if SERVER then
+                local pulseAmount = light.SerializableProperties[Identifier("pulseAmount")]
+                local pulseFrequency = light.SerializableProperties[Identifier("pulseFrequency")]
+                Networking.CreateEntityEvent(activeapsdata.apsitem, Item.ChangePropertyEventData(pulseAmount, light))
+                Networking.CreateEntityEvent(activeapsdata.apsitem, Item.ChangePropertyEventData(pulseFrequency, light))
+            end
             if Deep_Lua.HF.DoChance(activeapsdata.probability) then
                 Entity.Spawner.AddItemToRemoveQueue(activeapsdata.apstarget)
             end
@@ -16,8 +23,14 @@ Deep_Lua.APS = {
                 activeapsdata.triggered = true
                 Timer.Wait(function()
                     activeapsdata.apsitem.Condition = 0
-                    activeapsdata.apsitem.GetComponentString("LightComponent").pulseAmount = 0.0
-                    activeapsdata.apsitem.GetComponentString("LightComponent").pulseFrequency = 0
+                    light.pulseAmount = 0.0
+                    light.pulseFrequency = 0
+                    if SERVER then
+                        pulseAmount = light.SerializableProperties[Identifier("pulseAmount")]
+                        pulseFrequency = light.SerializableProperties[Identifier("pulseFrequency")]
+                        Networking.CreateEntityEvent(activeapsdata.apsitem, Item.ChangePropertyEventData(pulseAmount, light))
+                        Networking.CreateEntityEvent(activeapsdata.apsitem, Item.ChangePropertyEventData(pulseFrequency, light))
+                    end
                 end,5000)
                 Timer.Wait(function()
                     activeapsdata.apsitem.Condition = 100
