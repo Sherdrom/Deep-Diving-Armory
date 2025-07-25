3a8517dcf8047b27b4f25d58ba97697116047ebb
NotAlwaysTrue <2136846186@qq.com>
2025-07-25 08:44:56 +0800

    优化掉了一个参数

--- Lua/Scripts/APSList.lua
+++ Lua/Scripts/APSList.lua
@@ -9,7 +9,6 @@ Deep_Lua.APS = {
             Game.Explode(activeapsdata.apstarget.WorldPosition, 50, 30, 50, 50, 50, 0, 0)
             activeapsdata.apsitem.GetComponentString("LightComponent").pulseAmount = 1.0
             activeapsdata.apsitem.GetComponentString("LightComponent").pulseFrequency = 2.5
-            activeapsdata.apsitem.GetComponentString("LightComponent").Rotation = 1
             if Deep_Lua.HF.DoChance(activeapsdata.probability) then
                 Entity.Spawner.AddItemToRemoveQueue(activeapsdata.apstarget)
             end
@@ -19,7 +18,6 @@ Deep_Lua.APS = {
                     activeapsdata.apsitem.Condition = 0
                     activeapsdata.apsitem.GetComponentString("LightComponent").pulseAmount = 0.0
                     activeapsdata.apsitem.GetComponentString("LightComponent").pulseFrequency = 0
-                    activeapsdata.apsitem.GetComponentString("LightComponent").Rotation = 0
                 end,5000)
                 Timer.Wait(function()
                     activeapsdata.apsitem.Condition = 100
