b7580871cc8aed1a157a799e51b1b2b79dc8dc61
Sherdrom <525826386@qq.com>
2025-07-30 21:44:10 +0800

    新增ThermalVisablePatch，用来决定NPC是否可被热成像检测

---
+++ .AssemblyCSharpSource/ThermalVisablePatch/SharedProject/SharedSource/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using HarmonyLib;
+using Barotrauma;
+
+[assembly: IgnoresAccessChecksTo("Barotrauma")]
+[assembly: IgnoresAccessChecksTo("DedicatedServer")]
+[assembly: IgnoresAccessChecksTo("BarotraumaCore")]
+
+namespace ThermalVisablePatch
+{
+    public partial class ThermalVisablePatch : IAssemblyPlugin
+    {
+        public Harmony? harmonyInstance;
+        public void Initialize()
+        {
+            // When your plugin is loading, use this instead of the constructor
+            // Put any code here that does not rely on other plugins.
+            harmonyInstance = new Harmony("ThermalVisablePatch");
+            LuaCsSetup.PrintCsMessage("[Deep Diving Armory] ThermalVisablePatch Initialized!");
+        }
+
+        public void OnLoadCompleted()
+        {
+            // After all plugins have loaded
+            // Put code that interacts with other plugins here.
+            harmonyInstance?.PatchAll();
+            LuaCsSetup.PrintCsMessage("[Deep Diving Armory] ThermalVisablePatch Loaded!");
+        }
+
+        public void PreInitPatching()
+        {
+            // Not yet supported: Called during the Barotrauma startup phase before vanilla content is loaded.
+        }
+
+        public void Dispose()
+        {
+            // Cleanup your plugin!
+            harmonyInstance?.UnpatchSelf();
+            LuaCsSetup.PrintCsMessage("[Deep Diving Armory] ThermalVisablePatch Disposed!");
+        }
+    }
+}
