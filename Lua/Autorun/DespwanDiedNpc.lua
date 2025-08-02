4be327831f09c8be340e00af18f44d660986883a
Sherdrom <525826386@qq.com>
2025-08-02 20:56:23 +0800

    注释掉print

--- Lua/Autorun/DespwanDiedNpc.lua
+++ Lua/Autorun/DespwanDiedNpc.lua
@@ -4,7 +4,7 @@ LuaUserData.MakeMethodAccessible(Descriptors["Barotrauma.Character"], "Despawn")
 LuaUserData.MakeMethodAccessible(Descriptors["Barotrauma.AICharacter"], "Despawn")
 
 Hook.Add("character.death", "characterDeath", function(character)
-   print(character.IsDead, character.CauseOfDeath.Type, CauseOfDeathType.Disconnected, character.SpeciesName)
+--    print(character.IsDead, character.CauseOfDeath.Type, CauseOfDeathType.Disconnected, character.SpeciesName)
    if (not character.IsDead or (character.CauseOfDeath.Type == CauseOfDeathType.Disconnected and GameMain.GameSession.Campaign ~= nil ) or (character.SpeciesName ~= CharacterPrefab.HumanSpeciesName) or (character.TeamID == CharacterTeamType.Team1)) then
        return
    end
