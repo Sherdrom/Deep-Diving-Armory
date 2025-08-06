37bce8a951609275b8055f999e59304defc57d68
NotAlwaysTrue <2136846186@qq.com>
2025-08-06 20:10:00 +0800

    修bug

--- Lua/Scripts/CorpseRemover.lua
+++ Lua/Scripts/CorpseRemover.lua
@@ -9,7 +9,7 @@ end, nil, false)
 Game.AddCommand("RemoveAllCorpses", "Force remove all corpses immediately", function()
     for __,character in pairs(Character.CharacterList) do
         if character.isDead then 
-            if character.Removed then return end
+            if character.Removed or c.AnimController.Limbs == nil then return end
             character.EnableDespawn = true
             character.DespawnNow(true) 
         end
@@ -19,7 +19,7 @@ end, nil, false)
 
 Hook.Add("character.death", "Deep_CR_AddtoList", function(c)
     if (not c.IsHuman) and (not forceremove) then return end
-    if c.Removed then return end
+    if c.Removed or c.AnimController.Limbs == nil then return end
     c.EnableDespawn = true
     c.DespawnNow(true)
 end)
