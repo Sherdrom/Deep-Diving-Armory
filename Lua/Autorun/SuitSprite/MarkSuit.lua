f13fe6bd8e438402e5be2c987e0b5b8f90c11646
peach <1902445394@qq.com>
2025-08-15 14:18:10 +0800

    mark衣服调整

--- Lua/Autorun/SuitSprite/MarkSuit.lua
+++ Lua/Autorun/SuitSprite/MarkSuit.lua
@@ -9,7 +9,7 @@ Hook.Add("MarkSuit_MaskOn", function(_, _, item)
     local itemComponent = item.GetComponentString("Wearable")
     if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(422,2,81,88)
-    itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.62,0.5)
+    itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.62,0.55)
     -- 移除战术设备时应当清空所有的执行状态
     for _ , executed in pairs(isExecuted) do
         for i, _ in pairs(executed) do
@@ -22,7 +22,7 @@ Hook.Add("MarkSuit_MaskOff", function(_, _, item)
     local itemComponent = item.GetComponentString("Wearable")
     if itemComponent.wearableSprites[1].Sprite == nil then return end
     itemComponent.wearableSprites[1].Sprite.SourceRect=Rectangle(422,94,81,88)
-    itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.59,0.5)
+    itemComponent.wearableSprites[1].Sprite.RelativeOrigin=Vector2(0.59,0.55)
     -- 移除战术设备时应当清空所有的执行状态
     for _ , executed in pairs(isExecuted) do
         for i, _ in pairs(executed) do
