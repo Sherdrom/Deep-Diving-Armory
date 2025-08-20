fc301d15ca032f3783b32bba6a2338d1e2c6588d
NotAlwaysTrue <2136846186@qq.com>
2025-08-21 01:12:34 +0800

    筒子本地化

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -10,9 +10,9 @@ if SERVER or not Game.IsMultiplayer then
     dofile(Deep_Lua.Path .. "/Lua/Scripts/deployables.lua")--Deployables
     dofile(Deep_Lua.Path .. "/Lua/Scripts/APSList.lua")--APSList
     dofile(Deep_Lua.Path .. "/Lua/Scripts/CorpseRemover.lua")--CR
-    --dofile(Deep_Lua.Path .. "/Lua/Scripts/RocketConfig.lua")--Rockets
     dofile(Deep_Lua.Path .. "/Lua/Scripts/missileconfigs.lua")--Missile configs, will load before missileframework
     dofile(Deep_Lua.Path .. "/Lua/Scripts/missileframework.lua")--Missile Main, will be override if Framework is already exist
+    
 end
 
 dofile(Deep_Lua.Path .. "/Lua/Scripts/Airburst.lua")--Airburst stuff, work both side
