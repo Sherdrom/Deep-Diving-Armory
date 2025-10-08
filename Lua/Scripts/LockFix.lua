3b405ef0e7ddf23d831f3996478ad03d0f778fd3
NotAlwaysTrue <2136846186@qq.com>
2025-10-08 19:13:31 +0800

    Upd: 更新了锁定补丁代码 CRITICAL: 建议立刻更新以修复部分物品仍然锁定问题

--- Lua/Scripts/LockFix.lua
+++ Lua/Scripts/LockFix.lua
@@ -1,7 +1,8 @@
 Hook.Add("roundStart", "Deep_LuaLockFixer", function()
     for item in Item.ItemList do
-        if  item.prefab.ContentPackage.Name == "Deep Diving Armory" and item.NonInteractable then
+        if  item.prefab.ContentPackage.Name == "Deep Diving Armory" and (item.NonInteractable or item.NonPlayerTeamInteractable) then
             item.NonInteractable = false
+            item.NonPlayerTeamInteractable = false
             item.Condition = 100
         end
     end
