90ab069a693007ff41509ccac105ceee8c48e248
NotAlwaysTrue <2136846186@qq.com>
2025-10-08 21:05:41 +0800

    New: 添加了Lua缺失警告

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -14,8 +14,10 @@ if SERVER or not Game.IsMultiplayer then
     dofile(Deep_Lua.Path .. "/Lua/Scripts/missileframework.lua")--Missile Main, will be override if Framework is already exist
     dofile(Deep_Lua.Path .. "/Lua/Scripts/LockFix.lua")--Locked Item Fixer
     dofile(Deep_Lua.Path .. "/Lua/Scripts/RefreshTool.lua")--Item Refresh Tool
-    
+    dofile(Deep_Lua.Path .. "/Lua/Scripts/NotLuaMissing.lua")--Not Lua Missing
+
 end
 
 dofile(Deep_Lua.Path .. "/Lua/Scripts/Airburst.lua")--Airburst stuff, work both side
 --dofile(Deep_Lua.Path .. "/Lua/Scripts/Alter_NVG.lua")
+
---
+++ Lua/Scripts/NotLuaMissing.lua
@@ -0,0 +1,9 @@
+Hook.Add("Deep_NotLuaMissing", "Deep_NotLuaMissing",
+    function(effect, deltaTime, item, targets, worldPosition)
+        local Character = targets[1]
+        local CharacterHealth = Character.CharacterHealth
+        if CharacterHealth.GetAfflictionStrengthByIdentifier("deep_missing_Lua", true) > 0 then
+            CharacterHealth.ReduceAfflictionOnAllLimbs("deep_missing_Lua", 1, nil, nil)
+        end
+    end
+)
