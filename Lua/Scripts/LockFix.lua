b303b2b9f14b96aeca25b7575f34064110d89a39
NotAlwaysTrue <2136846186@qq.com>
2025-10-08 19:22:37 +0800

    Upd: 撤销上个更改

--- Lua/Scripts/LockFix.lua
+++ Lua/Scripts/LockFix.lua
@@ -1,8 +1,7 @@
 Hook.Add("roundStart", "Deep_LuaLockFixer", function()
     for item in Item.ItemList do
-        if  item.prefab.ContentPackage.Name == "Deep Diving Armory" and (item.NonInteractable or item.NonPlayerTeamInteractable) then
+        if  item.prefab.ContentPackage.Name == "Deep Diving Armory" and item.NonInteractable then
             item.NonInteractable = false
-            item.NonPlayerTeamInteractable = false
             item.Condition = 100
         end
     end
