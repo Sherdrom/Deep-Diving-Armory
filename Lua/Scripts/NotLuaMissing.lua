cecd81d923e86a5921e35b04866776786daafbd6
NotAlwaysTrue <2136846186@qq.com>
2025-10-09 14:04:24 +0800

    Upd: Lua缺失警告代码和本地化更新

--- Lua/Scripts/NotLuaMissing.lua
+++ Lua/Scripts/NotLuaMissing.lua
@@ -1,9 +1,11 @@
 Hook.Add("Deep_NotLuaMissing", "Deep_NotLuaMissing",
     function(effect, deltaTime, item, targets, worldPosition)
-        local Character = targets[1]
-        local CharacterHealth = Character.CharacterHealth
-        if CharacterHealth.GetAfflictionStrengthByIdentifier("deep_missing_Lua", true) > 0 then
-            CharacterHealth.ReduceAfflictionOnAllLimbs("deep_missing_Lua", 1, nil, nil)
-        end
+        Timer.Wait(function()
+            local Character = targets[1]
+            local CharacterHealth = Character.CharacterHealth
+            if CharacterHealth.GetAfflictionStrengthByIdentifier("deep_missing_Lua", true) > 0 then
+                CharacterHealth.ReduceAfflictionOnAllLimbs("deep_missing_Lua", 1, nil, nil)
+            end
+        end, 50)
     end
 )
