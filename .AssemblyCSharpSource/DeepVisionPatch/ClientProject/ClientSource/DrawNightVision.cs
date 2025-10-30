fe7990b3ff529e29882a335b530f385be62b3334
Sherdrom <525826386@qq.com>
2025-10-30 11:01:57 +0800

    Enhance DrawNightVision: implement dynamic texture assignment based on item tags

--- .AssemblyCSharpSource/DeepVisionPatch/ClientProject/ClientSource/DrawNightVision.cs
+++ .AssemblyCSharpSource/DeepVisionPatch/ClientProject/ClientSource/DrawNightVision.cs
@@ -8,10 +8,54 @@ namespace DeepVisionPatch;
 [HarmonyPatch(typeof(GameScreen),nameof(GameScreen.DrawMap))]
 public class DrawNightVision
 {
-    public static Texture2D texture = DeepVisionPatch.nvTexture.GetTexture();
+    private static CreateNightVisionTexture _nvTexture = new CreateNightVisionTexture();
+
+    private static readonly Dictionary<string, CreateNightVisionTexture> nvColor = new Dictionary<string, CreateNightVisionTexture>
+    {
+        ["NVG_Green"] = DeepVisionPatch.nvTexture_Green     // 绿色夜视仪
+    };
+
     public static void Postfix(GameScreen __instance, GraphicsDevice graphics, SpriteBatch spriteBatch, double deltaTime)
     {
-        DeepVisionPatch.nvTexture.Update((float)deltaTime);
+        Character character = Character.Controlled;
+        if (character == null) return;
+        Item headItem = character.Inventory.GetItemInLimbSlot(InvSlotType.Head);
+        if (headItem == null) return;
+        IEnumerable<Item> headItems = headItem.ContainedItems; // 获取头盔内的item
+
+        // 头盔战术设备（夜视仪）的开关判断
+        foreach (KeyValuePair<ushort, bool> nightVisionStatus in NightVisionPatch.NightVisionStatus)
+        {
+            if (headItem != null && headItem.ID == nightVisionStatus.Key)
+            {
+                if (nightVisionStatus.Value == false)
+                {
+                    return;
+                }
+            }
+        }
+
+        // 以下通过_nvTexture和item的Tag来设置夜视仪的颜色
+        foreach (Item item in headItems)
+        {
+            if (item != null && item.HasTag("NightVision")) // 获取夜视仪item
+            {
+                // 通过字典设置_nvTexture
+                foreach (KeyValuePair<string, CreateNightVisionTexture> kvp in nvColor)
+                {
+                    if (item.HasTag(kvp.Key))
+                        _nvTexture = kvp.Value;
+                }
+            }
+        }
+
+        DrawNightVisionTexture(spriteBatch, deltaTime, graphics, _nvTexture);
+    }
+    
+    public static void DrawNightVisionTexture(SpriteBatch spriteBatch, double deltaTime, GraphicsDevice graphics, CreateNightVisionTexture nvTexture)
+    {
+        nvTexture.Update((float)deltaTime);
+        Texture2D texture = nvTexture.GetTexture();
         spriteBatch.Begin(SpriteSortMode.Deferred, null, GUI.SamplerState, null, GameMain.ScissorTestEnable);
         // 计算屏幕中心并以纹理中心为原点绘制
         var viewport = graphics.Viewport;
@@ -19,7 +63,7 @@ public class DrawNightVision
         if (texture != null)
         {
             var origin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
-            spriteBatch.Draw(texture, center, null, Color.White, 0f, origin, new Vector2(viewport.Width/texture.Width*1.3f,viewport.Height/texture.Height*1.1f), SpriteEffects.None, 0f);
+            spriteBatch.Draw(texture, center, null, Color.White, 0f, origin, new Vector2(viewport.Width / texture.Width * 1.3f, viewport.Height / texture.Height * 1.1f), SpriteEffects.None, 0f);
         }
         spriteBatch.End();
     }
