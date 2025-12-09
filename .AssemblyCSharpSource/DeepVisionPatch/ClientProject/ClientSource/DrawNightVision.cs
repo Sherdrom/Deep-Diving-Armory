57a19f80aa04722b68c0c30a7ef80cd2eda549ff
Sherdrom <525826386@qq.com>
2025-12-09 21:41:47 +0800

    稍微扩大了一些安全距离，希望能够解决黑边问题

--- .AssemblyCSharpSource/DeepVisionPatch/ClientProject/ClientSource/DrawNightVision.cs
+++ .AssemblyCSharpSource/DeepVisionPatch/ClientProject/ClientSource/DrawNightVision.cs
@@ -71,7 +71,10 @@ public class DrawNightVision
         if (texture != null)
         {
             var origin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
-            spriteBatch.Draw(texture, center, null, Color.White, 0f, origin, new Vector2(viewport.Width / texture.Width * 1.3f, viewport.Height / texture.Height * 1.1f), SpriteEffects.None, 0f);
+
+            // X轴和Y轴分别缩放以填充整个屏幕
+            var scaleVector = new Vector2(viewport.Width / texture.Width * 1.3f, viewport.Height / texture.Height * 1.3f);
+            spriteBatch.Draw(texture, center, null, Color.White, 0f, origin, scaleVector, SpriteEffects.None, 0f);
         }
         spriteBatch.End();
     }
