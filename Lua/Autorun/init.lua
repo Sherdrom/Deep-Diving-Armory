70c17fbbd441219915ab41367e5657ec6eeda57b
peach <1902445394@qq.com>
2026-04-24 15:40:52 +0800

    lua脚本制作的伤害衰减上线

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -24,6 +24,8 @@ dofile(Deep_Lua.Path .. "/Lua/Scripts/reloadPatch.lua")--换弹自动填充
 
 --敌人脑子修改
 dofile(Deep_Lua.Path .. "/Lua/Scripts/Enemy/EnemyBattleBehavior.lua")
+--伤害衰减
+dofile(Deep_Lua.Path .. "/Lua/Scripts/DamageFallOff/DamageFallOff.lua")
 
 --头盔贴图修改
 dofile(Deep_Lua.Path .. "/Lua/Scripts/HelmetTacSprite/6B47.lua")
