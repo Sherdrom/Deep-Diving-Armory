9678e87360bce516c39ef2495625a756dfadfb22
Sherdrom <525826386@qq.com>
2026-05-18 00:44:44 +0800

    补上一个HK416.lua槽位slot位置

--- Lua/Scripts/Gunsmith/Config/Weapons/Rifle/Rifle_Phy/HK416.lua
+++ Lua/Scripts/Gunsmith/Config/Weapons/Rifle/Rifle_Phy/HK416.lua
@@ -23,7 +23,7 @@ config.weapons.deep_hk416 = {
         right_rail = { slot = 2 },
         left_rail = { slot = 3 },
         optic = { slot = 4 },
-        muzzle = { slot = 10 }
+        muzzle = { slot = 5 }
     },
 
     -- 选填，可注释。UI 预览窗口微调，只影响 GunSmith 窗口里的预览，不影响背包/世界贴图。
