331df56d828170f9c4364028abcd5c6ce5674a76
peach <1902445394@qq.com>
2026-06-07 03:03:36 +0800

    AR平台新增枪托和手枪握把

--- Lua/Scripts/Gunsmith/Config/Parts/AR/Structural/PistolGrip.lua
+++ Lua/Scripts/Gunsmith/Config/Parts/AR/Structural/PistolGrip.lua
@@ -15,3 +15,45 @@ parts.deep_miad_pistol_grip = { type = "pistol_grip",
         scale = 1.0
     }
 }
+
+parts.deep_colta2_pistol_grip = { type = "pistol_grip",
+    nameKey = "deep.gunsmith.part.deep_colta2_pistol_grip",
+    provides = { "AR_pistol_grip" },
+    item = { identifier = "deep_colta2_pistol_grip" },
+    stats = { Ergonomics = 4 },
+    visual = { 
+        texture = texture,
+        source = { x = 6, y = 5, w = 55, h = 60 },
+        attachPoint = { x = 53, y = 2 },
+        order = 50,
+        scale = 1.0
+    }
+}
+
+parts.deep_tac_hollowedout_pistol_grip = { type = "pistol_grip",
+    nameKey = "deep.gunsmith.part.deep_tac_hollowedout_pistol_grip",
+    provides = { "AR_pistol_grip" },
+    item = { identifier = "deep_tac_hollowedout_pistol_grip" },
+    stats = { Ergonomics = 4 },
+    visual = { 
+        texture = texture,
+        source = { x = 70, y = 7, w = 55, h = 60 },
+        attachPoint = { x = 53, y = 2 },
+        order = 50,
+        scale = 1.0
+    }
+}
+
+parts.deep_stark_black_pistol_grip = { type = "pistol_grip",
+    nameKey = "deep.gunsmith.part.deep_stark_black_pistol_grip",
+    provides = { "AR_pistol_grip" },
+    item = { identifier = "deep_stark_black_pistol_grip" },
+    stats = { Ergonomics = 4 },
+    visual = { 
+        texture = texture,
+        source = { x = 402, y = 7, w = 52, h = 79 },
+        attachPoint = { x = 49, y = 16 },
+        order = 50,
+        scale = 1.0
+    }
+}
--- Lua/Scripts/Gunsmith/Config/Parts/AR/Structural/Stock.lua
+++ Lua/Scripts/Gunsmith/Config/Parts/AR/Structural/Stock.lua
@@ -16,3 +16,45 @@ parts.deep_ctr_fde_stock = { type = "stock",
         scale = 1.0
     }
 }
+
+parts.deep_m4ss_stock = { type = "stock",
+    nameKey = "deep.gunsmith.part.deep_m4ss_stock",
+    provides = { "AR_stock" },
+    item = { identifier = "deep_m4ss_stock" },
+    stats = { Ergonomics = 3 },
+    visual = { 
+        texture = texture,
+        source = { x = 16, y = 12, w = 112, h = 84 },
+        attachPoint = { x = 110, y = 13 },  -- 挂载点位相对source的位置
+        order = 30,
+        scale = 1.0
+    }
+}
+
+parts.deep_f93_pro_stock = { type = "stock",
+    nameKey = "deep.gunsmith.part.deep_f93_pro_stock",
+    provides = { "AR_stock" },
+    item = { identifier = "deep_f93_pro_stock" },
+    stats = { Ergonomics = 3 },
+    visual = { 
+        texture = texture,
+        source = { x = 157, y = 12, w = 122, h = 75 },
+        attachPoint = { x = 120, y = 13 },  -- 挂载点位相对source的位置
+        order = 30,
+        scale = 1.0
+    }
+}
+
+parts.deep_dde_black_stock = { type = "stock",
+    nameKey = "deep.gunsmith.part.deep_dde_black_stock",
+    provides = { "AR_stock" },
+    item = { identifier = "deep_dde_black_stock" },
+    stats = { Ergonomics = 3 },
+    visual = { 
+        texture = texture,
+        source = { x = 800, y = 450, w = 95, h = 76 },
+        attachPoint = { x = 95 , y = 10 },  -- 挂载点位相对source的位置
+        order = 30,
+        scale = 1.0
+    }
+}
